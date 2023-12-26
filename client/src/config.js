d6be0b02e2617ebb3bbc581934ab9fdd9f107407
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2023-12-26 13:19:34 +0530

    newcommit3

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
-let BASE_URL = "https://quotabackendnew.onrender.com/";
+let BASE_URL = "https://quotes-abackendnew.vercel.app/";
 let new_URL = "http://localhost:4000/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "https://quotabackendnew.onrender.com/";
+  BASE_URL = "https://quotes-abackendnew.vercel.app/";
 }
 
 export { BASE_URL };
